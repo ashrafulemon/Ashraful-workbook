a7bc6a8a47084328fe312a50046fc255383c7c79
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-02-23 21:24:59 +0600

    Update class 09_1.R

--- class 09_1.R
+++ class 09_1.R
@@ -3,9 +3,9 @@ y= rnorm(1)
 z= rnorm(1)
 
 if(x>y & x>z){
-                                           #  print(1212)                          
-  sprintf("X =%f is the Highst value",x)   #  work after print, but not work before print.
-                                           #  print(121)                             
+                                                                   
+  sprintf("X =%f is the Highst value",x)  
+                                                                      
 }else if(y>z){
   sprintf("Y =%f is the Highst value",y)
 }else{
