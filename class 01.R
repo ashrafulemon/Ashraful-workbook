53a9874862b559abe60fc3d86dada2961605f490
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-02-23 21:14:11 +0600

    Update class 01.R

--- class 01.R
+++ class 01.R
@@ -6,14 +6,7 @@ sum(5,6,7,1000)
 10%%3     #vagshes
 10%/%3    #integer division
 
-a= 10
-sqrt(a)
-sum =0
-for(i in 1:5){
-  print(i)
-  sum=sum+i
-}
-sum
+
 
 
 builtins()  #all function ***
@@ -26,7 +19,7 @@ a <- 5
 
 b=55; d=44
 
-#builtins(false # incomple function
+
 
 a=1000
 
