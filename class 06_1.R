94374b1a11014349f9d25c570345b7ea2e0084d8
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-02-23 21:19:41 +0600

    Update class 06_1.R

--- class 06_1.R
+++ class 06_1.R
@@ -8,22 +8,7 @@ lst3
 lst4= list(name=c("asd","sasd"),wife=c("sds","mary"),children=c(3,4),child.ages=list(c(4,7,9),c(3,4,5,6)))
 lst4
 
-lst4$name
-lst4$child.ages
-lst4$child.ages[2]
-lst4$child.ages[1:2]
-lst4[3]
-lst4[4]
 
-lst4[[4]][1]
-
-lst4[[5]]= list(school=c("as","asd"),exp=c(12,34))
-lst4
-lst4[[1]]=c("aaa","sdssss")
-lst4
-#lst4[[1]][1]=c("a","sd")      #not work?
-lst4[[1]][1]=list(c("a","sd"))  
-lst4
 
 
 
