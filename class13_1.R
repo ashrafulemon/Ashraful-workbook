206c5aa289989ed990d77133f1cafd90091fa572
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-03-14 15:26:17 +0600

    Add files via upload

---
+++ class13_1.R
@@ -0,0 +1,118 @@
+#skewness kartosis
+
+
+#chi- square
+
+
+a=rchisq(2000000,10)
+plot(a)
+plot(density(a))
+
+par(mfrow= c(2,2))
+
+a=rchisq(20,10)
+plot(density(a))
+
+a=rchisq(20,100000)
+plot(density(a))
+
+a=rchisq(2000000,10)
+plot(density(a))
+
+a=rchisq(2000000,100000)
+plot(density(a))
+
+
+
+
+#sample space
+#coin toss 10 times
+2^10
+
+s =c("h","t")
+p =c("h","t")
+
+n_coin=10
+for( i in 1:(n_coin-1)){
+  s= outer(s,p,paste0)
+  
+}
+s
+c(s)
+a=c(s)
+a
+
+s =c(1,2,3,4,5,6)
+p =c(1,2,3,4,5,6)
+
+n_dias=2
+for( i in 1:(n_dias-1)){
+  s= outer(s,p,paste0)
+  
+}
+s
+c(s)
+a=c(s)
+a
+
+
+
+##
+
+bb= a[which(lengths(regmatches(a,gregexpr("h",a)))==2)]
+
+
+## my work
+a[1]
+a[[1]]
+sum1=0
+for( i in 1:(n_coin-1)){
+  for(j in 10){
+    a[i][j]
+  }
+}
+
+
+
+
+
+
+
+
+
+#regression
+
+x=c(2,4,5,6,6,5,7,6,6,7)
+y=c(4,4,5,6,5,5,4,4,5,4) #not reletion
+y1= 2*x+5
+
+
+
+cor(x,y1)
+cor(x,y)
+
+a=function(b){
+  
+}
+
+sum1=0
+sx=0
+ssx=0
+ssy=0
+sy=0
+spxy=0
+n=length(x)
+for(i in 1:n){
+  sx= sx+x[i]
+  ssx= ssx+x[i]^2
+  ssy= ssy+y[i]^2
+  sy= sy+y[i]
+  spxy= spxy+(x[i]*y[i])
+  
+}
+v= spxy-(sx*sy)/n
+v1= sqrt((ssx-(sx^2)/n)(ssy-(sy^2)/n))
+
+
+
+
